99870434243102b625ca7d24a3c264376a36ddf5
maruf520 <md.maruf5201@gmail.com>
2021-09-16 19:33:05 +0600

    Add project files.

---
+++ Model.Base/BaseObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Base
+{
+    public class BaseObject
+    {
+        public long Id { get; set; }
+        public DateTimeOffset CreateDate { get; set; }
+        public DateTimeOffset ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
